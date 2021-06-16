4ea6e27119816b473c87e9a295aa1765e97fcc50
Jessica <jessicakchell@gmail.com>
2021-06-16 13:00:37 +0200

    created html, css, js files & worked on desktop hero

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// change styles on scroll js
+window.addEventListener('scroll', function () {
+	let header = document.querySelector('header');
+	let windowPosition = window.scrollY > 0;
+	header.classList.toggle('scrolling-active', windowPosition);
+});
+
+// Select element function
+const selectElement = function (element) {
+	return document.querySelector(element);
+};
+
+const menuToggler = selectElement('.menu-toggle');
+const body = selectElement('body');
+
+menuToggler.addEventListener('click', function () {
+	body.classList.toggle('open');
+});
